e5aa1dc6e4f056abc08884d754ed40eecc3b1abf
rabahalishah <muhammadrabah.ali46@gmail.com>
2024-06-12 10:11:42 +0500

    successfully created context and used

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,15 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def home_view(request, *args, **kwargs):
     print("args, kwargs: ", args, kwargs)
     print("REQUEST: ", request)
-    return render(request, "home.html", {})
+    my_context = {
+        "about_home": "This is my home",
+        "more_information": "This house is located in Los angelas"
+    }
+    return render(request, "home.html", my_context)
 
 
 def contact_view(request, *args, **kwargs):
     print("args, kwargs: ", args, kwargs)
     print("REQUEST: ", request)
     print(args, kwargs)
+    my_context = {
+        "about_us": "This is about us"
 
-    return render(request, "contact.html", {})
+    }
+    return render(request, "contact.html", my_context)
